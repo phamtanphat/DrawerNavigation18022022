98f0f1de6de5524061ce26364a040cc8a6a6fd21
Phat <phatdroid94@gmail.com>
2022-06-03 19:25:15 +0700

    finish

--- app/src/main/java/com/example/drawernavigation18022022/MainActivity.java
+++ app/src/main/java/com/example/drawernavigation18022022/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.drawernavigation18022022;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.os.Bundle;
 import android.text.Html;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
 
+        fragmentManager = getSupportFragmentManager();
+        navigationView.setItemIconTintList(null);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(Html.fromHtml("<font color=\"red\">Home</font>"));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -36,8 +42,33 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                drawerLayout.openDrawer(Gravity.START);
+                drawerLayout.openDrawer(Gravity.LEFT);
             }
         });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment fragment = null;
+                String name = "";
+                switch (item.getItemId()) {
+                    case R.id.item_home:
+                        fragment = new AndroidFragment();
+                        name = "Android";
+                        break;
+                    case R.id.item_messages:
+                        fragment = new IosFragment();
+                        name = "Ios";
+                        break;
+                }
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+                getSupportActionBar().setTitle(Html.fromHtml("<font color=\"red\">" + name + "</font>"));
+                drawerLayout.closeDrawer(Gravity.LEFT);
+                return true;
+            }
+        });
+
     }
 }
